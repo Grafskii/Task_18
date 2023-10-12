0a005c4e4abbad4249f264c0742657cf8bc25d3a
Grafskii <miti-h@yandex.ru>
2023-10-12 18:02:52 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/CardDeliveryOrderTest.java
@@ -0,0 +1,40 @@
+package ru.netology.rest;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryOrderTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyComplited() {
+        open("http://localhost:9999");
+        String city = "Волгоград";
+        int dayToAdd = 10;
+        int defaultAddedDays = 3;
+        $("[data-test-id='city'] input").setValue(city.substring(0, 2));
+        $$(".menu-item__control").findBy(text(city)).click();
+        $("[data-test-id='date'] input").click();
+        if (!generateDate(defaultAddedDays, "MM").equals(generateDate(dayToAdd, "MM"))) {
+            $("[data-step='1']").click();
+        }
+        $$(".calendar__day").findBy(text(generateDate(dayToAdd, "d"))).click();
+        $("[data-test-id='name'] input").setValue("Петров Иван");
+        $("[data-test-id='phone'] input").setValue("+79001002030");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + generateDate(dayToAdd, "dd.MM.yyyy")));
+    }
+}
