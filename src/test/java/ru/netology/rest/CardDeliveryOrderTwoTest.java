4f807c1a90cab034f4b3abfaf74be018c5e52f15
Grafskii <miti-h@yandex.ru>
2023-10-14 12:46:52 +0300

    two commit

--- src/test/java/ru/netology/rest/CardDeliveryOrderTwoTest.java
+++ src/test/java/ru/netology/rest/CardDeliveryOrderTwoTest.java
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class CardDeliveryOrderTest {
+public class CardDeliveryOrderTwoTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
